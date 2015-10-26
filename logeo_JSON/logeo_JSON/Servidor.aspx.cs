ba886753118366ba7acc1e2625acb965df27ec19
DePascual <carolinadepascualjimenez@gmail.com>
2015-10-26 21:24:45 +0100

    Entrada OK!!

--- logeo_JSON/logeo_JSON/Servidor.aspx.cs
+++ logeo_JSON/logeo_JSON/Servidor.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using logeo_JSON.App_Code.modelo;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace logeo_JSON
 {
@@ -12,7 +14,28 @@ namespace logeo_JSON
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DatosPeticionCliente recibidos = new JavaScriptSerializer().Deserialize<DatosPeticionCliente>(this.Request.Params["dato"]);
 
+            Boolean encontrado = (from unalinea in new StreamReader(this.Server.MapPath("ficheros/usuarios.txt")).ReadToEnd().Split(new char[] { '\r', '\n' }).Where(una => una.Length > 0)
+                                  let campoUsu = unalinea.Split(new char[] { ':' })[0].ToString()
+                                  let campoPass = unalinea.Split(new char[] { ':' })[1].ToString()
+                                  where recibidos.Login == campoUsu && recibidos.Password == campoPass
+                                  select true).SingleOrDefault();
+
+            string respuesta = "";
+            if (encontrado)
+            {
+                respuesta = "{\"codigo\":0,\"mensaje\":\"autentificacion correcta\"}";
+            }
+            else
+            {
+                respuesta = "{\"codigo\":1,\"mensaje\":\"no existe usuario o password\"}";
+            }
+
+            this.Response.ContentType = "application/json";
+            this.Response.Write(respuesta);
+            this.Response.Flush();
+            this.Response.End();
         }
     }
 }
