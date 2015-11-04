6d1024118da04289375ec38a7981d09079aa4cc3
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-04 21:36:51 +0100

    Subida1. No funciona

---
+++ AjaxFileReader/AjaxFileReader/Upload.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.IO;
+
+namespace AjaxFileReader
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //recuperar los datos de tipo FileReader mandados por el cliente...
+            // Page.Request.Files <=== coleccion de tipo HttpFileCollection, objetos de tipo HttpPostedFile
+
+            HttpPostedFile fichero = this.Request.Files[0];
+
+            Byte[] contenido = new Byte[fichero.ContentLength];
+
+            fichero.InputStream.Read(contenido, 0, fichero.ContentLength);
+
+            string objetoJSON = "";
+            try {
+                File.WriteAllBytes(Server.MapPath("~/App_Data/uploads/") + fichero.FileName, contenido);
+                objetoJSON = "{'codigo':0,'mensaje':'Upload del fichero OK!!!'}";
+            }
+            catch
+            {
+                objetoJSON = "{'codigo':1,'mensaje':'FALLO Upload del fichero OK...'}";
+            }
+            finally
+            {
+                this.Response.Write(objetoJSON);
+                this.Response.ContentType = "application/json";
+                this.Response.End();
+            }
+        }
+    }
+}
