e1c41c62a41c9504b5d75b6301da8115c4786c29
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-05 18:38:16 +0100

    Grabandooo

---
+++ AjaxFileReader/AjaxFileReader/App_Code/modelo/Usuario.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxFileReader.App_Code.modelo
+{
+    public class Usuario
+    {
+        private string __Nombre;
+        private string __Apellido;
+        private string __Direccion;
+        private string __NIF;
+        private string __CP;
+        private string __Provincia;
+
+        private string __Login;
+        private string __Password;
+
+        public string Nombre
+        {
+            get
+            {
+                return __Nombre;
+            }
+
+            set
+            {
+                __Nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return __Apellido;
+            }
+
+            set
+            {
+                __Apellido = value;
+            }
+        }
+
+        public string Direccion
+        {
+            get
+            {
+                return __Direccion;
+            }
+
+            set
+            {
+                __Direccion = value;
+            }
+        }
+
+        public string NIF
+        {
+            get
+            {
+                return __NIF;
+            }
+
+            set
+            {
+                __NIF = value;
+            }
+        }
+
+        public string CP
+        {
+            get
+            {
+                return __CP;
+            }
+
+            set
+            {
+                __CP = value;
+            }
+        }
+
+        public string Provincia
+        {
+            get
+            {
+                return __Provincia;
+            }
+
+            set
+            {
+                __Provincia = value;
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return __Login;
+            }
+
+            set
+            {
+                __Login = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return __Password;
+            }
+
+            set
+            {
+                __Password = value;
+            }
+        }
+    }
+}
--- AjaxFileReader/AjaxFileReader/Upload.aspx.cs
+++ AjaxFileReader/AjaxFileReader/Upload.aspx.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.IO;
+using AjaxFileReader.App_Code.modelo;
+using System.Web.Script.Serialization;
 
 namespace AjaxFileReader
 {
     public partial class Upload : System.Web.UI.Page
     {
+        private StreamWriter __escritorFichero_Usuarios;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //recuperar los datos de tipo FileReader mandados por el cliente...
             // Page.Request.Files <=== coleccion de tipo HttpFileCollection, objetos de tipo HttpPostedFile
 
             HttpPostedFile fichero = this.Request.Files[0];
+            String usuario = this.Request.Form["usuario"];
+            Usuario usuarioRecibido = new JavaScriptSerializer().Deserialize<Usuario>(usuario);
 
             Byte[] contenido = new Byte[fichero.ContentLength];
 
@@ -22,17 +28,33 @@ namespace AjaxFileReader
 
             string objetoJSON = "";
             try {
-                File.WriteAllBytes(Server.MapPath("~/App_Data/uploads/") + fichero.FileName, contenido);
-                objetoJSON = "{'codigo':0,'mensaje':'Upload del fichero OK!!!'}";
+                File.WriteAllBytes(Server.MapPath("~/App_Data/uploads/") + usuarioRecibido.NIF + "$" + fichero.FileName, contenido);
+
+                __escritorFichero_Usuarios = new StreamWriter(new FileStream(this.Server.MapPath("ficheros/usuarios.txt"), FileMode.Append, FileAccess.Write));
+
+                string cadena_a_guardar_Registro = usuarioRecibido.Nombre + ":"
+                                                + usuarioRecibido.Apellido + ":"
+                                                + usuarioRecibido.Direccion + ":"
+                                                + usuarioRecibido.NIF + ":"
+                                                + usuarioRecibido.CP + ":"
+                                                + usuarioRecibido.Provincia + ":"
+                                                + usuarioRecibido.Login + ":"
+                                                + usuarioRecibido.Password;
+
+                __escritorFichero_Usuarios.WriteLine(cadena_a_guardar_Registro);
+                __escritorFichero_Usuarios.Flush();
+                __escritorFichero_Usuarios.Close();
+
+                objetoJSON = "{\"codigo\":0,\"mensaje\":\"Upload del fichero OK!!!\"}";
             }
             catch
             {
-                objetoJSON = "{'codigo':1,'mensaje':'FALLO Upload del fichero OK...'}";
+                objetoJSON = "{\"codigo\":1,\"mensaje\":\"FALLO Upload del fichero OK...\"}";
             }
             finally
             {
-                this.Response.Write(objetoJSON);
                 this.Response.ContentType = "application/json";
+                this.Response.Write(objetoJSON);
                 this.Response.End();
             }
         }
