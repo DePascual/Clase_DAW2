469e70df0a2f19e2e8c4e2b302ae853477311908
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-09 21:33:36 +0100

    Sin funcionar

--- WorkerAjax2/WorkerAjax2/miWorker.js
+++ WorkerAjax2/WorkerAjax2/miWorker.js
@@ -1,12 +1,18 @@
-﻿self.addEventListener("message", leerFichero, false);
+﻿self.addEventListener("message", function (ev) {
+    var fichero = ev.data; //objeto de tipo File o Blob!!! no es un string
+    var lector = new FileReaderSync();
 
-function leerFichero() {
-    var reader = new FileReader();
+    /*lector.addEventListener("load", function () {
+        self.postMessage(lector.result);
+    }, false);*/
 
-    reader.onload = function () {
-
+    lector.onload = function () {
+        self.postMessage(lector.result);
     }
-}
+    lector.readAsText(fichero);
+}, false);
+
+
 
 
 
