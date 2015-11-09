444207bd094fe012bb6ac01c0f3f9dbb46e66bcf
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-09 17:28:56 +0100

    Haciendo Hilos

---
+++ WorkerAjax/WorkerAjax/miWorker.js
@@ -0,0 +1,12 @@
+﻿self.addEventListener("message", hacerPetAjax, false);
+
+function hacerPetAjax(ev) {
+    var petAjax = new XMLHttpRequest();
+    petAjax.onreadystatechange = function () {
+        if (petAjax.readyState == 4 && petAjax.status == 200) {
+            self.postMessage(JSON.stringify(petAjax.responseText));
+        }
+    };
+    petAjax.open('GET', ev.data, true);
+    petAjax.send();
+}
