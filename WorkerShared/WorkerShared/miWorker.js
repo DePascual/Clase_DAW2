eed4aa83dd8e40e693859cf9d59ba6f538fd53bb
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-09 21:09:38 +0100

    SharedWorker

---
+++ WorkerShared/WorkerShared/miWorker.js
@@ -0,0 +1,17 @@
+﻿self.addEventListener("connect", function (ev) {
+    var puerto = ev.ports[0]; //objeto de tipo MessagePort
+    puerto.start();
+
+    puerto.addEventListener("message", function (e) {
+        var petAjax = new XMLHttpRequest;
+
+        petAjax.onreadystatechange = function () {
+            if (petAjax.readyState == 4 && petAjax.status == 200) {
+                puerto.postMessage(petAjax.responseText);
+            }
+        }
+        petAjax.open('GET', e.data, true);
+        petAjax.send();
+    }, false);
+
+}, false);
