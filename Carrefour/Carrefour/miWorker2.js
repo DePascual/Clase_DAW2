54bf3602dea804132e313cee1834f20050faf550
DePascual <carolinadepascualjimenez@gmail.com>
2015-12-05 11:02:18 +0100

    Uno

---
+++ Carrefour/Carrefour/miWorker2.js
@@ -0,0 +1,14 @@
+﻿self.addEventListener("message", hacerPetAjax, false);
+
+function hacerPetAjax(event) {
+    var petAjax = new XMLHttpRequest();
+    petAjax.onreadystatechange = function () {
+        if (petAjax.readyState == 4 && petAjax.status == 200) {
+            //var localidades = petAjax.responseText;
+
+            self.postMessage(petAjax.responseText);
+        }
+    };
+    petAjax.open('GET', event.data, true);
+    petAjax.send();
+}
